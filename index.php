27741036663a317c75a246c71fe658ebdc7b34c1
jayawson <58492612+jayawson@users.noreply.github.com>
2020-06-10 11:41:49 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>JobHouse</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<style>
+
+.skill {
+    color: #333533;
+}
+
+.skill:hover {
+    
+    color: #00c851;
+}
+
+    </style>
+
+<body>
+    <!--Main Navigation-->
+    <header>
+
+        <!-- Navbar -->
+        <nav class="navbar justify-end fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+            <div class="container">
+
+                <!-- Brand -->
+                <a class="navbar-brand waves-effect" href="#" target="_blank">
+                    <img src="img/jobhouse-logo.png" alt="" style="height:35px;" height="40px" width="201px">
+                </a>
+
+                <!-- Collapse -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+                <!-- Links -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <!-- Left -->
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            
+
+                            <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <!-- Right -->
+                    <ul class="ml-5 navbar-nav nav-flex-icons">
+
+                        <li class="nav-item ml-8">
+                            <a href="signup.php" class="btn btn-success">SignUp</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="login.php" class="btn btn-success">Login</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+            </div>
+        </nav>
+        <!-- Navbar -->
+
+    </header>
+    <!--Main Navigation-->
+
+    <!--Main layout-->
+    <main class="mt-0 pt-0">
+
+        <div class="container">
+
+            <!--Section: Jumbotron-->
+            <section class="card blue-gradient wow fadeIn" id="intro">
+
+                <!-- Content -->
+                <div class=" mask card-body text-white text-center py-5 px-5 my-5">
+
+                    <h1 class="mb-4 mt-4 pt-8">
+                        <strong>We have experts to make your dream a reality.</strong>
+                    </h1>
+                    <p class="mb-md-4">
+                        <strong></strong>Over 100 skills accross the world at one place.</strong>
+                    </p>
+                    <p class="mb-4">
+                        1,000 jobs and still counting.
+                    </p>
+                    <a target="_blank" href="signup.php class="btn btn-outline-white btn-lg mt-4">Get Started
+            </a>
+
+                </div>
+                <!-- Content -->
+            </section>
+            <!--Section: Jumbotron-->
+
+            <!--Section: Cards-->
+            <section class="">
+
+                <!-- Heading & Description -->
+                <div class="wow fadeIn row">
+                    <!--Section heading-->
+                    <div class="col-6">
+                        <!-- <p>Get connected</p> -->
+                    </div>
+
+
+                </div>
+                <!-- Heading & Description -->
+                <div class="how-section1">
+                    <div class="row">
+                        <div class="col-md-6 how-img">
+                            <img src="img/hero-img.png" class="rounded-circle img-fluid" alt="" />
+                        </div>
+                        <div class="col-md-6 pt-6">
+                            <form class="form-inline md-form mb-4">
+                                <input class="form-control form-control-lg active-cyan-2 mr-sm-2" width="20" type="text" placeholder="Search" aria-label="Search">
+                                <button class="btn aqua-gradient btn-primary btn-lg my-0" type="submit">Search</button>
+                            </form>
+                            <h4 class="font-weight-bold pt-4">Find rewarding projects</h4>
+                            <h3 class="subheading">jobhouse is a great place to find more clients, and to run and grow your own freelance business.</h3>
+                            <p class="text-muted">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations.
+                                Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work. More and more success. The greater the success you have on projects, the more likely you are to get
+                                hired by clients that use GetLance.</p>
+
+                            <a href="signup.php" class="btn btn-success btn-block mt-3">become a freelancer now</a>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row mt-4">
+                        <div class="col-md-6 pt-4">
+                            <h4 class="font-weight-bold">Get served quickly</h4>
+                            <h4 class="subheading">jobhouse makes it easy to connect with clients and begin doing great work.</h4>
+                            <p class="text-muted">Streamlined hiring. GetLance’s sophisticated algorithms highlight projects you’re a great fit for. Top Rated and Rising Talent programs. Enjoy higher visibility with the added status of prestigious programs. Do substantial
+                                work with top clients. GetLance pricing encourages freelancers to use GetLance for repeat relationships with their clients.</p>
+                            <a href="signup.php" class="btn btn-success btn-block mt-3">join to post your project</a>
+
+                        </div>
+                        <div class="col-md-6 how-img">
+                            <img src="img/header.jpg" class="rounded-circle img-fluid" alt="" />
+                        </div>
+                    </div>
+                    <div class="row">
+                       
+                        <div class="col">
+                            <h4 class="font-weight-bold mb-4">Browse our skills</h4>
+                            <div class="row">
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fas fa-2x fa-pen-nib"></i></h3>
+                                            <p class="card-text">Design</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fa fa-2x fa-code"></i></h3>
+                                            <p class="card-text">Development</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fa fa-2x fa-pen    "></i></h3>
+                                            <p class="card-text">Writing</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fas fa-2x fa-microphone   "></i></h3>
+                                            <p class="card-text">Voiceover</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2" href="#">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fas fa-2x fa-camera   "></i></h3>
+                                            <p class="card-text">Photography</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fa fa-2x fa-video   "></i></h3>
+                                            <p class="card-text">Video</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fa fa-2x fa-search  "></i></h3>
+                                            <p class="card-text">Research</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fa fa-2x fa-database   "></i></h3>
+                                            <p class="card-text">Data Science</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fas fa-2x fa-network-wired   "></i></h3>
+                                            <p class="card-text">Networking</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1-12 m-2">
+                                    <div class="card btn">
+                                        <div class="card-body">
+                                            <h3 class="card-title skill"><i class="fa fa-2x fa-lock   "></i></h3>
+                                            <p class="card-text">Security</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+             
+
+                <hr class="mb-5">
+
+            
+            </section>
+            <!--Section: Cards-->
+
+        </div>
+    </main>
+    <!--Main layout-->
+
+    <!--Footer-->
+    <footer class="page-footer text-center font-small mdb-color darken-2 mt-4 wow fadeIn">
+
+        <div class="my p-5 z-depth-1 unique-color-dark">
+
+
+            <!--Section: Content-->
+            <section class="text-center white-text">
+
+                <!-- Section heading -->
+                <h2 class="font-weight-bold mb-4 pb-2 text-uppercase">How it Works</h2>
+                <!-- Section description -->
+                <p class="lead mx-auto mb-5">We provide a platform for both job owners and job seekers</p>
+
+                <!-- Grid row -->
+                <div class="row">
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-4">
+                        <i class="fa fa-upload fa-2x text-success" aria-hidden="true"></i>
+                        <h5 class="font-weight-bold my-4 text-uppercase">Upload Job</h5>
+                        <p class="mb-md-0 mb-5">Job owners post their job on the application for job seekers to apply.
+                        </p>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-4">
+                        <i class="fas fa-users fa-2x text-success"></i>
+
+                        <h5 class="font-weight-bold my-4 text-uppercase">Freelancers Apply</h5>
+                        <p class="mb-md-0 mb-5">Job seekers, in our platform called freelancers apply to these jobs for the clients to approve their application.
+                        </p>
+
+                    </div>
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-4">
+                        <i class="fa fa-2x fa-check-circle text-success" aria-hidden="true"></i>
+
+                        <h5 class="font-weight-bold my-4 text-uppercase">Contract Job</h5>
+                        <p class="mb-0">The client approves their application, and allows the freelancers to either accept or decline the offer.
+                        </p>
+
+                    </div>
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 mb-4">
+                        <i class="fas fa-dollar-sign fa-2x text-success "></i>
+                        <h5 class="font-weight-bold my-4 text-uppercase">Secured Payments</h5>
+                        <p class="mb-0">A secure platform for payment when the job is done.
+                        </p>
+
+                    </div>
+                    <!-- Grid column -->
+
+                </div>
+                <!-- Grid row -->
+
+            </section>
+            <!--Section: Content-->
+
+
+        </div>
+        <!-- Social icons -->
+
+        <!--Copyright-->
+        <div class="footer-copyright py-3">
+            © 2020 Copyright:
+            <a href="#" target="_blank"> jobhouse.com </a>
+        </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <!--/.Footer-->
+
+    <!--header-->
+
+    <!-- End your project here-->
+    <!-- jQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/js/mdb.min.js"></script>
+    <!-- Your custom scripts (optional) -->
+    <script type="text/javascript">
+    </script>
+</body>
+
+</html>
